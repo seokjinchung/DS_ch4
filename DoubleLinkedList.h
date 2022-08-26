42f788ac90c786b258ca1411fcacd0ba821b8d8e
Your Name <you@example.com>
2022-08-26 16:48:36 +0900

    2nd commit

---
+++ DoubleLinkedList.h
@@ -0,0 +1,17 @@
+typedef struct ListNode {
+
+char data[4];
+struct ListNode* llink;
+struct ListNode* rlink;
+} listNode;
+
+typedef struct {
+listNode* head;
+} linkedList_h;
+
+linkedList_h* createLinkedList_h(void);
+void printList(linkedList_h* DL);
+void insertNode(linkedList_h*DL, listNode* pre, char* x);
+void deleteNode(linkedList_h* DL, listNode* old);
+listNode* searchNode(linkedList_h* DL, char* x);
+
